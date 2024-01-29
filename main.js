9cb2734e0208280f5e07d396e9fec52cf07ff6f0
marcelorpc <marcelorpchaves@gmail.com>
2024-01-29 20:39:21 -0300

    add logic to draw bets

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 import { MegaSenaResults } from "./megaSenaResultList.js";
+const TEXTO_JOGOS_GERADOS = document.querySelector("#jogosGerados")
 
-console.log(MegaSenaResults[1].concurso)
+const GerarJogoAleatorio = (quantidadeNumeros, quantidadeJogos) => {
+  let jogoGerado = []
+  let todosJogosGerados = []
+
+  for(let i = 0; i < quantidadeJogos; i++){
+    for(let j = 0; j < quantidadeNumeros; j++) {
+      let sortearConcurso = Math.round(Math.random() * (MegaSenaResults.length - 1))
+      let sortearNumero = Math.round(Math.random() * 5)
+      jogoGerado[j] = MegaSenaResults[sortearConcurso].numeros[sortearNumero]
+    }
+    todosJogosGerados += "Jogo Sorteado: " + jogoGerado.join(" ") + "<br>" + "<br>";
+  }
+
+  TEXTO_JOGOS_GERADOS.innerHTML = todosJogosGerados;
+}
+
+GerarJogoAleatorio(6, 100)
